393c4687b0b7faddfd29222ed88cfd83a472cf15
Matt Ambroziak <mambroziak@users.noreply.github.com>
2024-10-22 08:24:54 -0400

    build tests

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(
-    name='jfrog-python-example',
+    name='jfrog-python-actions-example',
     version='1.0',
     description='Project example for building Python project with JFrog products.',
     author='JFrog',
-    author_email='jfrog@jfrog.com',
-    url='https://github.com/carmithersh/carmit-testing',
+    author_email='matthewam@jfrog.com',
+    url='https://github.com/mambroziak/jfrog-python-actions-example',
     packages=['pythonExample'],
     install_requires=['PyYAML>3.11', 'nltk'],
 )
